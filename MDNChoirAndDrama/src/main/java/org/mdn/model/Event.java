9f2f974dff49267e6766c2dc7b7a3bca73983c12
Vikranth Doosa <vickydoosa@gmail.com>
2016-10-01 23:18:51 -0700

    add events

---
+++ MDNChoirAndDrama/src/main/java/org/mdn/model/Event.java
@@ -0,0 +1,54 @@
+package org.mdn.model;
+
+import java.util.Date;
+
+/**
+ * Created on 10/1/2016.
+ */
+public class Event {
+    private String _id;
+    private String title;
+    private String description;
+    private Date startTime;
+    private Date endTime;
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+}
---
+++ MDNChoirAndDrama/src/main/webapp/WEB-INF/pages/admin/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Vikranth
+  Date: 10/1/2016
+  Time: 11:06 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>Signup</title>
+</head>
+<body>
+
+</body>
+</html>
